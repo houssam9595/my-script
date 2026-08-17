bfdb948153ceb68679faf0d6745e409b82723264
hzmarrou <zmarrouhoussam@email.com>
2026-08-17 06:02:06 +0100

    sql pplugin

--- setup.sh
+++ setup.sh
@@ -467,6 +467,48 @@ return {
    end,
   },
 
+  -- Database client: vim-dadbod
+  {
+    "tpope/vim-dadbod",
+    cmd = { "DB" },
+  },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = { "tpope/vim-dadbod" },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_show_database_icon = 1
+      vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/dadbod-ui"
+    end,
+    keys = {
+      { "<leader>du", "<cmd>DBUIToggle<CR>", desc = "Toggle DB UI" },
+      { "<leader>df", "<cmd>DBUIFindBuffer<CR>", desc = "Find DB buffer" },
+    },
+  },
+
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    ft = { "sql", "mysql", "plsql" },
+    config = function()
+      local cmp = require "cmp"
+      cmp.setup.filetype({ "sql", "mysql", "plsql" }, {
+        sources = cmp.config.sources({
+          { name = "vim-dadbod-completion" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+
 
 
   -- test new blink
